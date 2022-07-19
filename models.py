dc0504d4a5689dd45c315c06011b744b534c7cca
Zakariae Barakat <bara.zakariae@gmail.com>
2022-07-19 21:14:31 +0100

    feat: add experience and skill models

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Optional
 from uuid import UUID, uuid4
 from pydantic import BaseModel, EmailStr, Field
@@ -21,3 +22,15 @@ class AdminUpdateRequest(BaseModel):
     email : Optional[EmailStr]
     password: Optional[str]
     gender: Optional[Gender]
+
+class skill(BaseModel):
+    name : str = Field(...)
+    skillType : str = Field(...)
+
+class experience(BaseModel):
+    title : str = Field(...) 
+    company : str = Field(...)
+    startDate : date = Field(...)
+    endDate : date = Field(...)
+    period : str = Field(...)
+    description : str = Field(...)
