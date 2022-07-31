04a18e989b4900e890e7533f315f65c0353af8ec
Zakariae Barakat <z.barakat@alumni.aui.ma>
2022-07-31 15:12:46 +0100

    feat: add experience's end points

---
+++ schemas/experience.py
@@ -0,0 +1,23 @@
+from datetime import date
+from enum import Enum
+from pydantic import BaseModel, Field
+
+class Type(str, Enum):
+    internship = "internship"
+    parttime = "parttime"
+    fulltime = "fulltime"
+
+class Experience(BaseModel):
+    title : str = Field(...)
+    type : Type = Field(...)
+    company : str = Field(...)
+    startDate : date = Field(...)
+    endDate : date = Field(...)
+
+
+class createdExperience(Experience):
+    title: str
+    type: Type
+    company : str
+    duration : str
+
